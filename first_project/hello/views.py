91d9f473410ac166c2a4be94fbff19231763213b
Guizar636 <124812297+Guizar636@users.noreply.github.com>
2023-05-29 18:50:35 +0000

    new year

--- first_project/hello/views.py
+++ first_project/hello/views.py
@@ -5,5 +5,5 @@ from django.http  import HttpResponse
 # Create your views here.
 def index (request):
    return HttpResponse("Hola desde mi primera vista ⭐")
-def index (request):
+def author (request):
    return HttpResponse("Autor: Guizar Mario 😎")
